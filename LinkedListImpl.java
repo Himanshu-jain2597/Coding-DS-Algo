a2ea1586fe9bc0bf32bce4eae9be67c7989ad899
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2020-07-11 17:04:34 +0530

    LinkedList Impl

---
+++ LinkedListImpl.java
@@ -0,0 +1,37 @@
+
+
+public class LinkedListImpl {
+
+
+    Node head;
+
+    void add(int data)
+    {
+        Node toAdd = new Node(data);
+
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=toAdd;
+
+    }
+
+
+    static class Node {
+
+        int data;
+        Node next;
+    }
+
+
+    public Node(int data)
+    {
+
+        this.data=data;
+        next=null;
+    }
+
+
+}
