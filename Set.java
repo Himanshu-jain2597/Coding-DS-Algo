d609301eb6cdf647db4d7b509b94e5920194e7d3
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2020-07-13 11:34:21 +0530

    set in java

---
+++ Set.java
@@ -0,0 +1,23 @@
+
+
+public class set {
+
+
+
+// add mehid return boolean if element is present it returns true and another false
+
+
+// tree set gives in sorted order
+    public static void main(String[] args)
+    {
+        Set<String>fruits= new HashSet<>();
+         Set<String>fruits1= new TreeSet<>();
+        fruits.add("Apple");
+        fruits.add("banana");
+
+        fruits1.add("Apple");
+        fruits1.add("banana");
+
+        System.out.println("fruits")
+    }
+}
